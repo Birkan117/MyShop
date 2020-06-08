cb7599cfe4e9cef324f79587655c38428ffd62f0
Birkan117 <57661682+Birkan117@users.noreply.github.com>
2020-06-08 11:20:33 +0100

    OrderManager Pages sorted

--- MyShop/MyShop.Core/Contracts/IOrderService.cs
+++ MyShop/MyShop.Core/Contracts/IOrderService.cs
@@ -11,6 +11,9 @@ namespace MyShop.Core.Contracts
     public interface IOrderService
     {
         void CreateOrder(Order basOrder, List<BasketItemViewModel> basketItems);
+        List<Order> GetOrderList();
+        Order GetOrder(string Id);
+        void UpdateOrder(Order updatedOrder);
 
     }
 }
